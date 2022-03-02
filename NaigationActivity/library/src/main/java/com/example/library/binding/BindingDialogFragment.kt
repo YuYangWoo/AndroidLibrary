ed30a58daad355d055fdc743e770971f00e6d2bc
YuYangWoo <yuyw0712@naver.com>
2022-03-02 18:22:40 +0900

    feat: BindingDialogFragment 1. DataBinding add

---
+++ NaigationActivity/library/src/main/java/com/example/library/binding/BindingDialogFragment.kt
@@ -0,0 +1,27 @@
+package com.example.library.binding
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import com.example.library.base.BaseDialogFragment
+
+open class BindingDialogFragment<VB: ViewDataBinding>(private val layoutId: Int) : BaseDialogFragment() {
+    protected lateinit var binding: VB
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, layoutId, container, false)
+        init()
+        return binding.root
+    }
+
+    private fun init() {
+
+    }
+}
