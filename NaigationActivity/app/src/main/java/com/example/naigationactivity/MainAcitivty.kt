f470979aa76db5e162bf7e6fd7b5844500a5a761
YuYangWoo <yuyw0712@naver.com>
2022-03-02 17:49:18 +0900

    test: NavigationLibrary test

--- NaigationActivity/app/src/main/java/com/example/naigationactivity/MainAcitivty.kt
+++ NaigationActivity/app/src/main/java/com/example/naigationactivity/MainAcitivty.kt
@@ -1,12 +1,16 @@
 package com.example.naigationactivity
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import com.example.library.NavigationActivity
+import com.example.naigationactivity.databinding.ActivityMainBinding
 
-class MainAcitivty : AppCompatActivity() {
+class MainAcitivty : NavigationActivity<ActivityMainBinding>(R.layout.activity_main, R.id.fragment) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        NavigationActivity.longToast(this, "빠끄빠끄")
+        initToolBar()
+    }
+
+    private fun initToolBar() {
+        setSupportActionBar(binding.toolbar)
     }
 }
